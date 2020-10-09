b559ac67bf54d199f976f819deadbc637e0e2064
AaronMg2000 <rudy.gopal.2000@gmail.com>
2020-10-09 14:52:11 -0600

    Creación de Rutas y login
    Creación de Rutas y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { LoginComponent } from './Components/login/login.component';
+import { Page404Component } from './Components/page404/page404.component';
+import { PensumComponent } from './Components/pensum/pensum.component';
+import { PublicacionComponent } from './Components/publicacion/publicacion.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { RestablecerComponent } from './Components/restablecer/restablecer.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'Inicio', component: InicioComponent},
+  {path: 'Registro', component: RegistroComponent},
+  {path: 'Pensum/:id', component: PensumComponent},
+  {path: 'Usuario/:id', component: UsuarioComponent},
+  {path: 'Restablecer', component: RestablecerComponent},
+  {path: 'Publicacion/:id', component: PublicacionComponent},
+  {path: 'Perfil/:id', component: UsuarioComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { PensumComponent } from './Components/pensum/pensum.component';
+import { PerfilComponent } from './Components/perfil/perfil.component';
+import { PublicacionComponent } from './Components/publicacion/publicacion.component';
+import { Page404Component } from './Components/page404/page404.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { RestablecerComponent } from './Components/restablecer/restablecer.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
+import { PrincipalComponent } from './Components/Nabvars/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    PensumComponent,
+    PerfilComponent,
+    PublicacionComponent,
+    Page404Component,
+    RegistroComponent,
+    RestablecerComponent,
+    UsuarioComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
